2a469273fe1c8f657bf693b2d5468abed37defbc
zabit-musalaev <61622553+zabit-musalaev@users.noreply.github.com>
2020-03-22 20:55:26 +0300

    turvdag

---
+++ turvdagestan/js/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+  var mySwiper = new Swiper ('.swiper-container', {
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+    },
+
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+  })
+
+  var next = $('.swiper-button-next');
+  var prev = $('.swiper-button-prev');
+  var bullets = $('.swiper-pagination');
+
+  next.css('left', prev.width() + 10 + bullets.width() + 10)
+  bullets.css('left', prev.width() + 10);
+
+});
